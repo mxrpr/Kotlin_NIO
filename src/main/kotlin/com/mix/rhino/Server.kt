3b9978f89ffdbb4fcc27820e9db3dd6f938ec4a8
mixer <ferenc.tollas@gmail.com>
2019-03-17 08:34:31 +0100

    Server.kt: add documentation to code

--- src/main/kotlin/com/mix/rhino/Server.kt
+++ src/main/kotlin/com/mix/rhino/Server.kt
@@ -1,6 +1,15 @@
 package com.mix.rhino
 
-
+/**
+ * Initialise the NIO server. Server responsibility is
+ * to open a ServerSocket and listen for connections
+ *
+ * In order to receive notification about content receiving, an IMessageReceiver
+ * must be registered. In case of error, the server will notify the client via
+ * IErrorReceiver.
+ *
+ * @param portToListen Port number where the server will listen
+ */
 fun initNIOServer(
         portToListen : Int = 4545,
         block: com.mix.rhino.NetworkService.()-> Unit
@@ -11,6 +20,18 @@ fun initNIOServer(
     return networkService
 }
 
+/**
+ * Initialise the NIO client. Opens a connection to the server, keeps the
+ * channel open. Client application can send message to the server with the
+ * help of the `sendMessage` method.
+ *
+ * In order to receive notification about content receiving, an IMessageReceiver
+ * must be registered. In case of error, the server will notify the client via
+ * IErrorReceiver.
+ *
+ * @param serverPort Port number where the server is listening
+ * @param serverAddress Address of the server
+ */
 fun initNIOClient (
         serverPort : Int = 4545,
         serverAddress : String = "127.0.0.1",
